6f1f084c9e5cd6d1f41b812ed6a0a6d942b8225f
Guzman <guzmanvigliecca@gmail.com>
2021-04-07 12:10:10 -0400

    Added evaulation function

---
+++ game/evaluation.py
@@ -0,0 +1,43 @@
+
+from truco import Game
+from alphabeta_player import AlphaBetaPlayer
+
+
+
+def evaluate():
+    reflexScore = 0
+    alphaBetaScore = 0
+
+    for i in range(100):
+
+        game = Game()
+
+        player1 = AlphaBetaPlayer("Reflex", game, 1)
+        player2 = AlphaBetaPlayer("AlphaBeta", game, 6)
+
+        game.setPlayers(player1, player2)
+        game.initGameState()
+
+        state = game.getState()
+        while not state.getWinner():
+
+            currentPlayer = state.playerTurn
+            state = currentPlayer.play(state)
+
+        game.calculateEnvidoWinner(state)
+
+        print("\n" + state.getWinnerName() + " has won truco \n")
+        print("Final scores:")
+        state.printScores()
+
+        reflexScore += state.getPlayerScore(player1)
+        alphaBetaScore += state.getPlayerScore(player2)
+
+    print("Reflex final score: " + str(reflexScore))
+    print("AlphaBeta final score: " + str(alphaBetaScore))
+
+
+
+
+
+evaluate()
